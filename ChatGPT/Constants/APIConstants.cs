c97805d2c97fb1715197a890359afbabca36c62e
Daniel Monettelli <danielmonetelli@hotmail.com>
2023-02-11 21:13:14 -0500

    🚀 [Code] Changed the string name of the "OpenAIToken" constant

--- ChatGPT/Constants/APIConstants.cs
+++ ChatGPT/Constants/APIConstants.cs
@@ -3,7 +3,7 @@
 	public static class APIConstants
 	{
 		public const string OpenAIUrl = "https://api.openai.com/";
-		public const string OpenAIToken = "API_KEY_HERE";
+		public const string OpenAIToken = "OPENAI_API_KEY_HERE";
 
 		public const string OpenAIEndpoint_Completions = "v1/completions";
 		public const string OpenAIEndpoint_Generations = "v1/images/generations";
