586bded463888a0ce77f87bbc4c172d9a0ceddd6
Daniel Monettelli <danielmonetelli@hotmail.com>
2024-02-29 20:12:40 -0500

    🐛 [Code] Fixed HTTP 403 error on AskQuestion due to incorrect request body parameters

--- ChatGPT/Models/CompletionRequest.cs
+++ ChatGPT/Models/CompletionRequest.cs
@@ -2,18 +2,18 @@
 
 namespace ChatGPT.Models
 {
-	public class CompletionRequest
-	{
-		[JsonPropertyName("model")]
-		public string Model { get; set; } = "text-davinci-003";
+    public class CompletionRequest
+    {
+        [JsonPropertyName("model")]
+        public string Model { get; set; } = "gpt-3.5-turbo-instruct";
 
-		[JsonPropertyName("prompt")]
-		public string Prompt { get; set; }
+        [JsonPropertyName("prompt")]
+        public string Prompt { get; set; }
 
-		[JsonPropertyName("temperature")]
-		public double Temperature { get; set; } = 0;
+        [JsonPropertyName("temperature")]
+        public double Temperature { get; set; } = 0;
 
-		[JsonPropertyName("max_tokens")]
-		public int MaxTokens { get; set; } = 100;
-	}
+        [JsonPropertyName("max_tokens")]
+        public int MaxTokens { get; set; } = 100;
+    }
 }
